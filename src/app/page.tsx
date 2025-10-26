13a47ca085b95c64a45445b6917356d79cc65e9c
Suvra Samajder <suvra.samajder@wellnessconnection.care>
2025-10-26 19:51:15 +0530

    Fetch and display users from the database in the main page component
    - Updated the `Page` component to be asynchronous and retrieve a list of users from the Prisma database.
    - Added a mapping function to display user names, defaulting to "No name" if not available.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { User } from "@/generated/prisma/client";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  console.log(users);
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      {users.map((user: User) => (
+        <div key={user.id}>{user.name ?? "No name"}</div>
+      ))}
       <Button>Click me</Button>
     </div>
   );
